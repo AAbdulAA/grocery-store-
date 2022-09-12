751226d2c08799d2b9f869882743695aa390a39d
AAbdulAA <Abdulhaleem.Al-otaibi@edu.newton.se>
2022-09-12 08:47:05 +0200

    Initial commit

---
+++ GroceryStore/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+//                                          -------------------PseduCode------------------                      ---------------------ShoppingCart-----------------
+
+// Welcome the user to the app
+// Show available product to the user 
+// Show what is in the shopping cart to user 
+// Aske the user for what they want to buy, or if they want to leave the store
+// IF the product the user want to buy is available 
+// Put the product in the shoppin cart 
+// Remove the product from available products
+// ELSE
+//Tell the user that the product is not available
+
+
+
+
+Console.WriteLine("                                   HELLO & WELLCOME!                    "); 
---
+++ README.md
@@ -0,0 +1,2 @@
+# grocery-store CODE ALONG
+ Small console APP that is a simple Grocery store 
